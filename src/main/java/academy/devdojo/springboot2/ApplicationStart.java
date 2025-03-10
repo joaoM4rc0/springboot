ad9a095a366e407f13da4b6a463e50db361e0645
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-03-10 14:26:11 -0300

    utilizando o Hot Swap

--- src/main/java/academy/devdojo/springboot2/ApplicationStart.java
+++ src/main/java/academy/devdojo/springboot2/ApplicationStart.java
@@ -1,13 +1,11 @@
 package academy.devdojo.springboot2;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class ApllicationStart {
+public class ApplicationStart {
     public static void main(String[] args) {
-        SpringApplication.run(ApllicationStart.class, args);
+        SpringApplication.run(ApplicationStart.class, args);
     }
 }
--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -3,7 +3,6 @@ package academy.devdojo.springboot2.controller;
 import academy.devdojo.springboot2.dominio.Anime;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +14,21 @@ import java.util.List;
 @RequestMapping("anime")
 @Log4j2
 public class AnimeController {
-    @Autowired
     private DateUtil dateUtil;
+
+    public AnimeController(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
+    }
+
     //localhost:8080/anime/list
     @GetMapping(path = "list")
     public List<Anime> list() {
         log.info(dateUtil.formatDate(LocalDateTime.now()));
         return List.of(new Anime("DBZ", 300), new Anime("berserk", 200));
+    }
+    @GetMapping(path = "list2")
+    public List<Anime> list2() {
+        log.info(dateUtil.formatDate(LocalDateTime.now()));
+        return List.of(new Anime("naruto", 700), new Anime("boku no hero", 200), new Anime("jujutsu kaisen", 80));
     }
 }
