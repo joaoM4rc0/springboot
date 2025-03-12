c8ebf10325447f72d16ea18cb310e77de380299a
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-03-12 14:23:53 -0300

    metodo put

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -37,9 +37,14 @@ public class AnimeController {
     public ResponseEntity<Anime> save(@RequestBody Anime anime) {
         return new ResponseEntity<>(animeService.save(anime),HttpStatus.CREATED);
     }
-    @DeleteMapping
+    @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable long id) {
         animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Anime anime) {
+        animeService.replace(anime);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
